d0532457efe279b3b372068b77cead536c084989
nguyenhuuduc-coder <122002649+nguyenhuuduc-coder@users.noreply.github.com>
2025-06-24 23:55:12 +0900

    Fix IsRound function by using raycast

--- Assets/_Scripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerMovement.cs
@@ -4,20 +4,23 @@ public class PlayerMovement : MonoBehaviour
 {
     protected Rigidbody2D body;
     protected Animator anim;
+    protected BoxCollider2D boxCollider;
     [SerializeField] protected float speed = 5f;
-    [SerializeField] protected float jumpSpeed = 10f;
-    protected bool isGounded;
+    [SerializeField] protected float jumpSpeed = 7f;
+    [SerializeField] LayerMask groundLayer;
+    [SerializeField] LayerMask wallLayer;
 
 
     private void Awake()
     {
-        LoadRb();
-        LoadAnim();
+        this.LoadRb();
+        this.LoadAnim();
+        this.LoadCollider();
     }
 
     private void Update()
     {
-        Movement();
+        this.Movement();
     }
 
     protected virtual void LoadRb()
@@ -28,9 +31,14 @@ public class PlayerMovement : MonoBehaviour
     {
         this.anim = GetComponent<Animator>();
     }
+    protected virtual void LoadCollider()
+    {
+        this.boxCollider = GetComponent<BoxCollider2D>();
+    }
 
     protected virtual void Movement()
     {
+        //basic left right movement
         float horizontalInput = Input.GetAxis("Horizontal");
         this.body.velocity = new Vector2(horizontalInput * this.speed, this.body.velocity.y);
                        
@@ -38,10 +46,10 @@ public class PlayerMovement : MonoBehaviour
         if (horizontalInput > 0.01f)
             transform.localScale = Vector3.one;
         else if (horizontalInput < -0.01f)
-            transform.localScale = new Vector3(-1f, 1, 1);
+            transform.localScale = new Vector3(-1f, 1f, 1f);
 
         //This is for jumping
-        if (Input.GetKey(KeyCode.Space) && isGounded == true)
+        if (Input.GetKey(KeyCode.Space))
             this.Jump();
 
         //Add animation for running movement
@@ -51,24 +59,42 @@ public class PlayerMovement : MonoBehaviour
             this.anim.SetBool("IsRun", false);
 
         //Add animation for Jumping when press jump or falling
-            this.anim.SetBool("IsGrounded", isGounded);
+            this.anim.SetBool("IsGrounded", this.IsGrounded());
+
+
+
 
 
     }
 
     protected virtual void Jump()
     {
-        this.body.velocity = new Vector2(this.body.velocity.x, this.jumpSpeed);
-        this.anim.SetTrigger("IsJump");
-        this.isGounded = false;
+        if(this.IsGrounded())
+        {
+            this.body.velocity = new Vector2(this.body.velocity.x, this.jumpSpeed);
+            this.anim.SetTrigger("IsJump");
+        }
+        else if(this.OnWall() && !this.IsGrounded())
+        {
+
+        }
     }
 
 
     protected void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Ground")
-        {
-            this.isGounded = true;
-        }
+
+    }
+
+    protected virtual bool IsGrounded()
+    {
+        RaycastHit2D rayCastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, this.groundLayer);        
+        return rayCastHit.collider != null;
+    }
+
+    protected virtual bool OnWall()
+    {
+        RaycastHit2D rayCastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, new Vector2(transform.localScale.x, 0), 0.1f, this.wallLayer);
+        return rayCastHit.collider != null;
     }
 }
